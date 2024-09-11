8a56c25b8bc03b45d9c231319e427a6625dbf5ba
LorKamiT <lorkamimau@gmail.com>
2024-09-11 02:49:20 -0600

    up

--- src/components/Guias/snippets/codeSnippets.js
+++ src/components/Guias/snippets/codeSnippets.js
@@ -75,7 +75,7 @@ export default App;`,
       code: `npm i next-themes
 npm i clsx
 npm i react-icons --save
-npm install -D prettier prettier-plugin-tailwindcss
+npm i -D prettier prettier-plugin-tailwindcss
       `,
     },
   },
